78b102f663949e2eee07a146f47911e13c973894
heitorlssd <heitorlssd@gmail.com>
2026-09-05 13:35:31 -0300

    Adição de barra de pesquisa

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,21 +1,62 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new();
+
     public ListaProduto()
     {
         InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
 
-        lst_produtos.ItemsSource = await App.Db.GetAll();
+        List<Produto> produtos = await App.Db.GetAll();
+
+        lista.Clear();
+
+        foreach (Produto p in produtos)
+        {
+            lista.Add(p);
+        }
     }
 
-    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    private void ToolbarItem_Clicked(
+        object sender,
+        EventArgs e)
     {
-        await Navigation.PushAsync(new NovoProduto());
+        Navigation.PushAsync(new NovoProduto());
+    }
+
+    private async void txt_search_TextChanged(
+        object sender,
+        TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> produtos;
+
+        if (string.IsNullOrWhiteSpace(q))
+        {
+            produtos = await App.Db.GetAll();
+        }
+        else
+        {
+            produtos = await App.Db.Search(q);
+        }
+
+        foreach (Produto p in produtos)
+        {
+            lista.Add(p);
+        }
     }
 }
